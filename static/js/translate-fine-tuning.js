e5337ecba43d760bf79178f76a801375158f6cde
iJimday <ijimday@gmail.com>
2026-04-23 20:30:26 +0800

    Update translate-fine-tuning.js

--- static/js/translate-fine-tuning.js
+++ static/js/translate-fine-tuning.js
@@ -1,6 +1,6 @@
 translate.selectLanguageTag.show = false //设置不显示语言选择的select下拉框
 translate.language.setLocal('chinese_simplified'); //设置本地语种为简体中文
-translate.language.translateLanguagesRange = ['chinese_simplified']; //只翻译简体中文
+
 translate.language.setUrlParamControl('lang'); //根据URL传参控制以何种语种显示
 translate.ignore.text.push('中文'); //翻译时忽略指定的文字不翻译
 translate.ignore.text.push('English'); //翻译时忽略指定的文字不翻译
@@ -9,5 +9,6 @@ translate.nomenclature.append('chinese_simplified', 'english', `
 			其他=Others
 			关于=About
 			小戴=Jimday
-			就不告诉你=I won't tell you
+			就不告诉你‼️=I won't tell you️‼️
+			Jimday | 小戴=Jimday
 		`); //自定义翻译术语
