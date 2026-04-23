069936b173623493051b75ccc1f6706cdbad77ba
iJimday <ijimday@gmail.com>
2026-04-23 23:01:48 +0800

    Update translate-fine-tuning.js

--- static/js/translate-fine-tuning.js
+++ static/js/translate-fine-tuning.js
@@ -1,7 +1,7 @@
+translate.language.setUrlParamControl('lang');//根据URL传参控制以何种语种显示
 translate.selectLanguageTag.show = false //设置不显示语言选择的select下拉框
 translate.language.setLocal('chinese_simplified');//设置本地语种为简体中文
 translate.language.translateLanguagesRange = ['chinese_simplified'];//只翻译简体中文
-translate.language.setUrlParamControl('lang');//根据URL传参控制以何种语种显示
 translate.setAutoDiscriminateLocalLanguage();//设置用户第一次用时，自动识别其所使用的语种进行切换
 translate.ignore.text.push('中文');//翻译时忽略指定的文字不翻译
 translate.ignore.text.push('English');//翻译时忽略指定的文字不翻译
